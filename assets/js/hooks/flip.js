0f2c37f6479e8e154c4fec350dcba4666e67c849
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-09 15:10:46 +0200

    Basic - not working flip

--- assets/js/hooks/flip.js
+++ assets/js/hooks/flip.js
@@ -1,7 +1,41 @@
 const Flip = {
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
+  child: null,
+  currentBounds: null,
+  mounted() {
+    this.child = this.el.children[0];
+  },
+  beforeUpdate() {
+    this.currentBounds = this.child.getBoundingClientRect();
+  },
+  updated() {
+    const newBounds = this.child.getBoundingClientRect();
+
+    const deltaX = this.currentBounds.left - newBounds.left;
+    const deltaY = this.currentBounds.top - newBounds.top;
+    const deltaW = this.currentBounds.width / newBounds.width;
+    const deltaH = this.currentBounds.height / newBounds.height;
+
+    this.el.animate(
+      [
+        {
+          transformOrigin: "top left",
+          transform: `
+        translate(${deltaX}px, ${deltaY}px)
+        scale(${deltaW}, ${deltaH})
+      `,
+        },
+        {
+          transformOrigin: "top left",
+          transform: "none",
+        },
+      ],
+      {
+        duration: 10000,
+        easing: "ease-in-out",
+        fill: "both",
+      }
+    );
+  },
   destroyed() {},
   disconnected() {},
   reconnected() {},
